b685339feb4b939c04923c4cc5881ba16110e095
yimuguo <guoyimu@gmail.com>
2015-03-06 18:27:20 -0800

    80%finishe

--- vec2pat.py
+++ vec2pat.py
@@ -1,16 +1,35 @@
 #!/usr/bin/python
 import re
-import binascii
 vec_file='vec.txt'
 with open(vec_file,'r') as vec_data:
     vec=vec_data.read().splitlines()
 vec=vec[0]
-#bin=binascii.unhexlify(vec)
-#bin=re.findall('.',bin)
-hex=re.findall('..',vec)
-bin=[]
-for x in hex:
-    a=binascii.a2b_hqx(x)
-    y="{0:8b}".format(int(x,16))
-    bin.append(y)
-print len(vec)
+########Get all vector seperated into list#######
+hexlist=re.findall('..',vec)
+del vec
+binlist=[]
+########Convert Hex to Bin
+for x in hexlist:
+    #y=hex_to_binary(x)
+    y=bin(int(x,16))[2:].zfill(8)
+    #y="{0:8b}".format(int(x,16))
+    binlist.append(y)
+########Split Binary List into final output list to print
+l=[]
+for x in binlist:
+    y=re.findall('.',x)
+    l.append(y)
+
+pat=open('pat.txt','w+')
+pat.write('import tset bstar, bstop, mack, nack, noop, readt, sack, wridt;\n')
+pat.write('\n')
+pat.write('vector       ( $tset, CSCL, CSDA)\n{\nstart_label Write_B0_to_B50:\n')
+i=0
+for x in l:
+    pat.write('write_byte_%d:\t\t\t\t\t//%s\n\t\t\t> wridt\t\t1\t%s\n' % (i,hexlist[i],l[i][0]))
+    for m in xrange(1,len(x)):
+        pat.write('\t\t\t>\t-\t\t1\t%s\n' % l[i][m])
+    pat.write('\t\t\t> sack\t\t1\tL\n')
+    i+=1
+pat.close()
+#print l
