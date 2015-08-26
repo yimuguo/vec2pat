170177c14b85da5f708676a20914876ac8da2137
yimuguo <guoyimu@gmail.com>
2015-08-25 18:53:12 -0700

    1

--- VC5_910/reg2pat_910.py
+++ VC5_910/reg2pat_910.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python
 import re
-vec_file=".\\VC5_910\\registerhex_910\\vec.txt"
-with open(vec_file,'r') as vec_data:
-    vec=vec_data.read().splitlines()
-vec=vec[0]
+vec_file = ".\\VC5_910\\registerhex_910\\vec.txt"
+with open(vec_file, 'r') as vec_data:
+    vec = vec_data.read().splitlines()
+vec = vec[0]
 
-########Write Binary Data to Pattern Format
-def write8b(name,hexinput,pat):
-    binlist=[]
+# Write Binary Data to Pattern Format
+
+
+def write8b(name, hexinput, pat):
+    binlist = []
     '''
     if type(hexinput) is list:
         for x in hexinput:
@@ -16,37 +18,38 @@ def write8b(name,hexinput,pat):
             binlist.append(z)
     '''
     if type(hexinput) is str:
-        y=bin(int(hexinput,16))[2:].zfill(8)
-        binlist=re.findall('.',y)
+        y = bin(int(hexinput, 16))[2:].zfill(8)
+        binlist = re.findall('.', y)
     elif type(hexinput) is int:
-        y="{0:b}".format(hexinput).zfill(8)
-        binlist=re.findall('.',y)
+        y = "{0:b}".format(hexinput).zfill(8)
+        binlist = re.findall('.', y)
     else:
-        print 'Input type wrong, either string or number'
-    pat.write('%s:\t\t\t\t\t//%s\n' % (name,hexinput))
+        print('Input type wrong, either string or number')
+    pat.write('%s:\t\t\t\t\t//%s\n' % (name, hexinput))
     pat.writelines('\t\t\t> wridt\t\t1\t%s;\n' % item for item in binlist)
     pat.write('\t\t\t> sack\t\t1\tL;\n')
 
-########Get all vector seperated into list#######
-hexlist=re.findall('..',vec)
+######## Get all vector seperated into list#######
+hexlist = re.findall('..', vec)
 del vec
 
-def bytex2y(x,y):
-    startbit=int(x)
-    address='D4'
-    pat=open('.\\VC5_910\\patterns\\register%sto%s.atp' %(x,y),'w+')
+
+def bytex2y(x, y):
+    startbit = int(x)
+    address = 'D4'
+    pat = open('..\\VC5_910\\patterns\\register%sto%s.atp' % (x, y), 'w+')
     pat.write('import tset bstar, bstop, mack, nack, noop, readt, sack, wridt;\n')
     pat.write('\n')
     pat.write('vector       ( $tset, CSCL, CSDA)\n{\nstart_label Write_B%s_to_B%s:\n' % (x,y))
     pat.write('\t\t\t> noop\t\t1\t1;\nrepeat 10\t> noop\t\t1\t1;\n\t\t\t> bstar\t\t1\t1;\n')
     
-    write8b('Slave_address',address,pat)
-    write8b('Prowrite_CMD','00',pat)
-    write8b('Start_w_byte',startbit,pat)
-    i=x
-    for i in xrange(x,y):
-        write8b('write_byte_%d' % i,hexlist[i],pat)
-        i+=1
+    write8b('Slave_address', address, pat)
+    write8b('Prowrite_CMD', '00', pat)
+    write8b('Start_w_byte', startbit, pat)
+    i = x
+    for i in range(x, y):
+        write8b('write_byte_%d' % i, hexlist[i], pat)
+        i += 1
     pat.write('\t\t\t> bstop\t\t1\t0;\n')
     pat.write('repeat 5\t> noop\t\t1\t1;\nhalt\t\t>\t-\t\t-\t-;\n\t\t\t>\t-\t\t-\t-;\n}')
     pat.close()
