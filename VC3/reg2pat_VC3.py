e50ff7aecec3e8ca4567a89f9aa110ae25319dc0
yimuguo <guoyimu@gmail.com>
2015-08-26 19:06:25 -0700

    aug.26.night

--- VC3/reg2pat_VC3.py
+++ VC3/reg2pat_VC3.py
@@ -46,11 +46,35 @@ def write8b(name, hexinput, pat):
 
 ######## Get all vector separated into list#######
 
+def read8b(name, hexinput, pat):
+    binlist = []
+    '''
+    if type(hexinput) is list:
+        for x in hexinput:
+            y=bin(int(x,16))[2:].zfill(8)
+            z=re.findall('.',y)
+            binlist.append(z)
+    '''
+    if type(hexinput) is str:
+        y = bin(int(hexinput, 16))[2:].zfill(8)
+        binlist = re.findall('.', y)
+    elif type(hexinput) is int:
+        y = "{0:b}".format(hexinput).zfill(8)
+        binlist = re.findall('.', y)
+    else:
+        print('Input type wrong, either string or number')
+    for x in binlist:
+        if x == 0:
+            x = 'L'
+    pat.write('%s:\t\t\t\t\t//%s\n' % (name, hexinput))
+    pat.writelines('\t\t\t> readt\t\t1\t%s;\n' % item for item in binlist)
+    pat.write('\t\t\t> mack\t\t1\t0;\n')
 
-def bytex2y(x, y):
+
+def writeregx2y(x, y):
     startbit = int(x)
     address = 'D4'
-    pat = open('.\\patterns\\register%sto%s.atp' % (x, y), 'w+')
+    pat = open('.\\patterns\\write%sto%s.atp' % (x, y), 'w+')
     pat.write('import tset bstar, bstop, mack, nack, noop, readt, sack, wridt;\n')
     pat.write('\n')
     pat.write('vector       ( $tset, CSCL, CSDA)\n{\nstart_label Write_B%s_to_B%s:\n' % (x,y))
@@ -68,10 +92,34 @@ def bytex2y(x, y):
     pat.close()
 
 
-#bytex2y(0,208)
-bytex2y(0,51)
-bytex2y(51, 101)
-bytex2y(101,150)
-bytex2y(150,208)
-#bytex2y(200,208)
-#print l
+
+writeregx2y(0,51)
+writeregx2y(51, 101)
+writeregx2y(101,150)
+writeregx2y(150,208)
+
+
+def readregx2y(x, y):
+    startbit = int(x)
+    address = 'D4'
+    pat = open('.\\patterns\\read%sto%s.atp' % (x, y), 'w+')
+    pat.write('import tset bstar, bstop, mack, nack, noop, readt, sack, wridt;\n')
+    pat.write('\n')
+    pat.write('vector       ( $tset, CSCL, CSDA)\n{\nstart_label Write_B%s_to_B%s:\n' % (x,y))
+    pat.write('\t\t\t> noop\t\t1\t1;\nrepeat 10\t> noop\t\t1\t1;\n\t\t\t> bstar\t\t1\t1;\n')
+
+    write8b('Slave_address', address, pat)
+    write8b('Prowrite_CMD', '00', pat)
+    write8b('Start_r_byte', startbit, pat)
+    pat.write('\t\t\t> noop\t\t1\t1;\n\t\t\t> bstar\t\t1\t1;\n')
+    write8b('Restart','D5',pat)
+
+    i = x
+    for i in range(x, y):
+        read8b('read_byte_%d' % i, hexlist[i], pat)
+        i += 1
+    pat.write('\t\t\t> bstop\t\t1\t0;\n')
+    pat.write('repeat 5\t> noop\t\t1\t1;\nhalt\t\t>\t-\t\t-\t-;\n\t\t\t>\t-\t\t-\t-;\n}')
+    pat.close()
+
+readregx2y(51,101)
