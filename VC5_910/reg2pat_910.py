73daea9775f678dbf80e629cd34f47f1519780ff
yimuguo <guoyimu@gmail.com>
2015-11-11 19:45:53 -0800

    05

--- VC5_910/reg2pat_910.py
+++ VC5_910/reg2pat_910.py
@@ -1,9 +1,23 @@
 ﻿#!/usr/bin/python
 import re
-vec_file = ".\\VC5_910\\registerhex_910\\vec.txt"
-with open(vec_file, 'r') as vec_data:
-    vec = vec_data.read().splitlines()
-vec = vec[0]
+import os
+import sys
+import glob
+from subprocess import call
+
+try:
+    for file in os.listdir(".\\patternsD4\\code910\\"):
+        if file.endswith(".txt"):
+            ConfigFile = open(os.path.join(".\\patternsD4\\code910\\", file), mode='rb')
+            vec_data = ConfigFile.read()
+            vec_string = vec_data.decode('utf-16')
+            vec_data = vec_string.split('\r\n')
+            for line in vec_data:               # Read Code into List
+                if line[:12] == "<Binary Hex=" and len(line) > 400:
+                    hexlist = [line[13:-3][i:i + 2] for i in range(0, len(line[13:-3]), 2)]
+                    break
+except RuntimeError:
+    sys.exit('No Configuration File in Code Folder')
 
 # Write Binary Data to Pattern Format
 
@@ -29,34 +43,37 @@ def write8b(name, hexinput, pat):
     pat.writelines('\t\t\t> wridt\t\t1\t%s;\n' % item for item in binlist)
     pat.write('\t\t\t> sack\t\t1\tL;\n')
 
-######## Get all vector seperated into list#######
-hexlist = re.findall('..', vec)
-del vec
-
 
 def bytex2y(x, y):
     startbit = int(x)
     address = 'D4'
-    pat = open('.\\VC5_910\\pattern\\register%sto%s.atp' % (x, y), 'w+')
+    pat_file = '.\\patternsD4\\code910\\register%sto%s.atp' % (x, y)
+    # os.makedirs(os.path.dirname(pat_file), exist_ok=True)
+    pat = open(pat_file, 'w+')
     pat.write('import tset bstar, bstop, mack, nack, noop, readt, sack, wridt;\n')
     pat.write('\n')
-    pat.write('vector       ( $tset, CSCL, CSDA)\n{\nstart_label Write_B%s_to_B%s:\n' % (x,y))
+    pat.write('vector       ( $tset, CSCL, CSDA)\n{\nstart_label Write_B%s_to_B%s:\n' % (x, y))
     pat.write('\t\t\t> noop\t\t1\t1;\nrepeat 10\t> noop\t\t1\t1;\n\t\t\t> bstar\t\t1\t1;\n')
 
     write8b('Slave_address', address, pat)
     write8b('Prowrite_CMD', '00', pat)
     write8b('Start_w_byte', startbit, pat)
-    i = x
+    # i = x
     for i in range(x, y):
         write8b('write_byte_%d' % i, hexlist[i], pat)
         i += 1
     pat.write('\t\t\t> bstop\t\t1\t0;\n')
     pat.write('repeat 5\t> noop\t\t1\t1;\nhalt\t\t>\t-\t\t-\t-;\n\t\t\t>\t-\t\t-\t-;\n}')
     pat.close()
-#bytex2y(0,208)
-#bytex2y(0,51)
+# bytex2y(0,208)
+bytex2y(0, 51)
 bytex2y(51, 101)
-#bytex2y(101,150)
-#bytex2y(150,200)
-#bytex2y(200,208)
-#print l
+bytex2y(101, 150)
+bytex2y(150, 200)
+bytex2y(200, 208)
+
+os.chdir('.')
+for prg_file in glob.glob("*FT*.xls"):
+    workbook = prg_file
+call('apc register*.atp -digital_inst hsd100200 -extended -pinmap_workbook ' + workbook)
+os.system('\"apc register*.atp -digital_inst hsd100200 -extended -pinmap_workbook \"' + '\"' + workbook + '\"')
