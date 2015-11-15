6a8df555743381af2338c9651467d23c71785431
yimuguo <guoyimu@gmail.com>
2015-11-14 23:28:53 -0800

    get VC5 write/read. Implemented library unity.

--- Lib/vc5_summary.py
+++ Lib/vc5_summary.py
@@ -5,31 +5,52 @@ import re
 
 
 class VC5Get(object):
-    def __init__(self, search_path='.//'):                  # Read Summary File
+    conf = []
+    output_freq = []
+    conf_enable = [0, 0, 0, 0]
+    # conf_string = []
+    vdd = []
+    i2c_address = 'D4'
+
+    def __init__(self, search_path='.//', process=1, vco_mon=False):                  # Read Summary File
         os.chdir(search_path)
         try:
             for file in glob.glob("*summary*.txt"):
                 filename = file
                 self.summary_file = open(filename, 'r')
+                self.vco_mon = vco_mon
+                if process == 1:
+                    self.process_file()
+                break
         except (RuntimeError, TypeError, NameError):
             sys.exit('No Summary Txt File Present')
 
     def process_file(self):
-        conf_string = []
-        conf = []
-        output_freq = []
-        conf_enable = [0, 0, 0, 0]
         search_for = ['CLK0', 'CLK1', 'CLK2', 'CLK3', 'CLK4']
         for line in self.summary_file:
             if any(x in line for x in search_for):      # Read Frequency Across Configs
                 line = re.split('\s+', line)
-                output_freq.append(line[1])
+                self.output_freq.append(line[1])
+                self.vdd.append(line[3])
             elif ("Configuration" in line) and (len(line) > 20):
-                line = re.split('\s+', line)            # Read Config Hex Strings and Correct I2C Address for Timing Commander
+                line = re.split('\s+', line)            # Read Config Hex Strings and Correct I2C Address for TC
                 if line[2] == 'E0':
                     line[2] = '60'
+                    self.i2c_address = 'D0'
                 elif line[2] == 'E1':
                     line[2] = '61'
                 line = line[2:]
-                conf_string = ' '.join(line)
-                conf.append(conf_string)
+                if self.vco_mon is False:
+                    line[0x11] = '3F'
+                    line[0x1c] = str(hex(int(line[0x1c], 16) & 0b01111111).zfill(2))[2:]
+                    line[0x1d] = str(hex(int(line[0x1d], 16) & 0b11111101).zfill(2))[2:]
+                    line[0x16] = str(hex(int(line[0x16], 16) & 0b11110111).zfill(2))[2:]
+                # self.conf_string = ' '.join(line)
+                self.conf.append(line)
+
+        for i in range(0, 4):
+            for x in range(int(len(self.output_freq) / 4)*i, (1+i)*int(len(self.output_freq) / 4)):
+                if self.output_freq[x] != '-----':
+                    self.conf_enable[i] = 1
+                    break
+
