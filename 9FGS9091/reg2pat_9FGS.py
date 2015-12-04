6675febeb6859efc351938657683c5ce5f8ba8e2
yimuguo <guoyimu@gmail.com>
2015-12-03 16:51:33 -0800

    bug on i2c address D0

--- 9FGS9091/reg2pat_9FGS.py
+++ 9FGS9091/reg2pat_9FGS.py
@@ -7,6 +7,11 @@ OutputsHiz_hex = ['00', '5F', '87', '78', '44', '87', '78', '44', '87', '78', '4
 os.chdir('.\\patterns\\')
 wbytes_pat(OutputsHiz_hex, 'OutputHiz', I2C_ADD)
 
+Default002_string = '00	4F	8F	01	44	8F	01	44	8F	01	44	8F	01	44	85	85	00	00	' \
+             '0C	80	00 00	00	00	81	00	A0	F9	AF	00	0F	32	19	19	00	00	F5	FD'
+Default002 = re.split('\s+', Default002_string)
+wbytes_pat(Default002, 'Default', I2C_ADD)
+
 # ============OTP Section==============
 w1byte_pat(40, '28', 'OTP_burn_start', I2C_ADD)
 w1byte_pat(40, '20', 'OTP_burn_stop', I2C_ADD)
