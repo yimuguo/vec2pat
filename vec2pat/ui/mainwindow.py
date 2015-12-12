76d129d2d3e0b834ed0fdde4e1518191f17cfb0e
yimuguo <guoyimu@gmail.com>
2015-12-11 17:02:43 -0800

    starting GUI

--- vec2pat/ui/mainwindow.py
+++ vec2pat/ui/mainwindow.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt4 import QtGui
+from PyQt4 import QtGui, QtCore
 
 
 class Window(QtGui.QMainWindow):
@@ -9,7 +9,15 @@ class Window(QtGui.QMainWindow):
         self.setGeometry(100, 100, 500, 300)
         self.setWindowTitle("MMT J750 Pattern Generation Tool")
         self.setWindowIcon(QtGui.QIcon('IDTlogo.png'))
+        # self.show()
+        self.home()
+
+    def home(self):
+        btn = QtGui.QPushButton("Quit", self)
+        # btn.clicked.connect(QtCore.QCoreApplication.instance().quit)
+        QtCore.QObject.connect(btn, QtCore.SIGNAL('clicked()'), quit())
         self.show()
+
 
 app = QtGui.QApplication(sys.argv)
 GUI = Window()
