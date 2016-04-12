81290e019edb3d8ff6cb407ad7b5dc8ba3ed5fe6
yimuguo <guoyimu@gmail.com>
2016-04-11 21:19:06 -0700

    Load UI directly using uic

--- vec2pat/examples/9FGS9091/9FGS9091_OD_Char.py
+++
@@ -1,6 +0,0 @@
-from vec2pat import reg2pat
-import os
-os.chdir('S:\\Test_Eng\\J750_HW_SW\\9FGSxxxx\\9FGS9091\\patterns\\OD_CHAR\\')
-
-for od_ratio in range(8, 100):
-    reg2pat.w1byte_pat(33, od_ratio, 'OD_%d' % od_ratio, 'D0')
